89a9ef2752af9b67f4b84e02a266d6ad72554c2d
Tran Hung <tranhung@Trans-MacBook-Air.local>
2020-10-12 13:30:30 -0500

    pattern_ID_display

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let nameInput = document.getElementById("username")
 let nameSearch = document.getElementById("nameSearch")
 let profileInfo = document.getElementById("profileInfo")
+let projectDetail = document.getElementById("projectDetail")
 
 
 nameSearch.addEventListener("click", () => {
@@ -29,7 +30,36 @@ function showProjects(json) {
                     <p>Completed on ${project.completed}</p>
                     <p>Craft name ${project.craft_name}</p>
                     <img src = ${project.first_photo.square_url}>
+                    <button onclick="showState('${project.pattern_id}')">Choose me</button>
                 </li>`
     })
     profileInfo.insertAdjacentHTML('beforeend', projects.join(''))
+}
+function showState(id){
+    if (id == "null"){
+        window.alert("Please choose a project with a pattern selected in Project info.")
+    }else{
+    let url = `https://api.ravelry.com/patterns/${id}.json`
+    function getAPI(url) {
+        const headers = new Headers();
+        headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
+        return fetch(url, { method: 'GET', headers: headers }).then(function(response) {
+            return response.json();
+        }).then(function(json) { 
+            console.log(json)
+            return json; 
+        }).catch((error)=>{
+            window.alert("Please choose a project with a pattern selected in Project info.")
+        }); 
+    }
+    let json = getAPI(url)
+    .then(function(json){
+        showProjectDetail(json)
+    })}
+
+}
+
+function showProjectDetail(json){
+    let details = `<p>${json.pattern.yardage_max}</p>`
+    projectDetail.innerHTML = details
 }
