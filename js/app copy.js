d80c2e762eed5ab4e1f3a84b7b9c945114f9ddc8
brodri4 <70927606+brodri4@users.noreply.github.com>
2020-10-12 12:04:20 -0400

    App Js commit

---
+++ js/app copy.js
@@ -0,0 +1,20 @@
+let nameInput = document.getElementById("username")
+let nameSearch = document.getElementById("nameSearch")
+
+nameSearch.addEventListener("click", () => {
+    let name = nameInput.value
+    let url = `https://api.ravelry.com/projects/${name}/list.json`
+    function getAPI(url) {
+        const headers = new Headers();
+        headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
+        return fetch(url, { method: 'GET', headers: headers }).then(function(response) {
+            return response.json();
+        }).then(function(json) { 
+            console.log(json)
+            return json; 
+        });
+    }
+    getAPI(url)
+})
+
+
