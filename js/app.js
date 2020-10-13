f7e625f9d5eae78470b7c7fbd1723ea4cda3a344
Borinquen  Rodriguez <boris@Borinquens-MacBook-Pro.local>
2020-10-13 12:28:13 -0400

    added phrase for length

--- js/app.js
+++ js/app.js
@@ -1,65 +1,87 @@
-let nameInput = document.getElementById("username")
-let nameSearch = document.getElementById("nameSearch")
-let profileInfo = document.getElementById("profileInfo")
-let projectDetail = document.getElementById("projectDetail")
+let nameInput = document.getElementById("username");
+let nameSearch = document.getElementById("nameSearch");
+let profileInfo = document.getElementById("profileInfo");
+let projectDetail = document.getElementById("projectDetail");
+let modal = document.getElementById("simpleModal");
+let closeBtn = document.getElementById("closeBtn");
 
+const sort1 = lengths.sort((a,b)=> a.length - b.length)
+console.log(sort1)
 
 nameSearch.addEventListener("click", () => {
-    let name = nameInput.value
-    let url = `https://api.ravelry.com/projects/${name}/list.json`
-    function getAPI(url) {
-        const headers = new Headers();
-        headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
-        return fetch(url, { method: 'GET', headers: headers }).then(function(response) {
-            return response.json();
-        }).then(function(json) { 
-            console.log(json)
-            return json; 
-        }); 
-    }
-    let json = getAPI(url)
-    .then(function(json){
-        showProjects(json)
-    })
-
-})
+  let name = nameInput.value;
+  let url = `https://api.ravelry.com/projects/${name}/list.json`;
+  getAPI(url).then(function (json) {
+    showProjects(json);
+  });
+});
 
 function showProjects(json) {
-    let projects = json.projects.map((project) => {
-        return `<li class = "projectInfo">
-                    <p>Completed on ${project.completed}</p>
-                    <p>Craft name ${project.craft_name}</p>
+  let projects = json.projects.map((project) => {
+    return `<li class = "projectInfo">
+                    <p>${project.name}</p>
                     <img src = ${project.first_photo.square_url}>
-                    <button onclick="showState('${project.pattern_id}')">Choose me</button>
-                </li>`
+                    <button onclick="showState('${project.pattern_id}', '${project.first_photo.square_url}', '${project.name.replace(/'/g,"\\'")}')">Choose me</button>
+                </li>`;
+  });
+  profileInfo.insertAdjacentHTML("beforeend", projects.join(""));
+}
+function showState(id, img, name) {
+  if (id == "null") {
+    window.alert(
+      "Please choose a project with a pattern selected in Project info."
+    );
+  } else {
+    let url = `https://api.ravelry.com/patterns/${id}.json`;
+    getAPI(url).then(function (json) {
+      showProjectDetail(json, img, name);
+    });
+  }
+  // call function to open modal
+  openModal();
+}
+
+function showProjectDetail(json, img, name) {
+  
+  let lenghtText = generateText(json.pattern.yardage_max)
+  let details = `<div id = "imageDetails">
+                    <p>${json.pattern.yardage_max}</p>
+                    <img src = '${img}'>
+                    <p id = "imageDetailsText">"${name}"</p>
+                    <p>${lengthText}</p>
+                    </div>`;
+  projectDetail.innerHTML = details;
+}
+// add function to show the modal
+function openModal() {
+  modal.style.display = "block";
+}
+// add listion to close modal
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+//function to fetch data from ravelry
+function getAPI(url) {
+  const headers = new Headers();
+  headers.append("Authorization", "Basic " + btoa(username + ":" + password));
+  return fetch(url, { method: "GET", headers: headers })
+    .then(function (response) {
+      return response.json();
     })
-    profileInfo.insertAdjacentHTML('beforeend', projects.join(''))
+    .then(function (json) {
+      console.log(json);
+      return json;
+    });
 }
-function showState(id){
-    if (id == "null"){
-        window.alert("Please choose a project with a pattern selected in Project info.")
-    }else{
-    let url = `https://api.ravelry.com/patterns/${id}.json`
-    function getAPI(url) {
-        const headers = new Headers();
-        headers.append('Authorization', 'Basic ' + btoa(username + ":" + password));
-        return fetch(url, { method: 'GET', headers: headers }).then(function(response) {
-            return response.json();
-        }).then(function(json) { 
-            console.log(json)
-            return json; 
-        }).catch((error)=>{
-            window.alert("Please choose a project with a pattern selected in Project info.")
-        }); 
+
+function generateText(size){
+  for(i=0; i < sort1.length; i++){
+    if (size > sort1[i].length && size < sort1[i+1].length){
+      return `Your project used more yarn than the length of ${sort1[i].name}`
     }
-    let json = getAPI(url)
-    .then(function(json){
-        showProjectDetail(json)
-    })}
 
-}
-
-function showProjectDetail(json){
-    let details = `<p>${json.pattern.yardage_max}</p>`
-    projectDetail.innerHTML = details
+  }
 }
---
+++ js/lengths.js
@@ -0,0 +1,42 @@
+// all length units are yards
+
+let lengths = [
+    { 
+    name: "Manhattan Island",
+    dimension: "width",
+    length: 4048
+    },
+    {
+    name: "airport runway",
+    dimension: "length",
+    length: 3663.605
+    },
+    {
+    name: "football field",
+    dimension: "length",
+    length: 120.297
+    },
+    {
+    name: "Symphony of hte Seas",
+    description: "world's largest cruise ship",
+    dimension: "length",
+    length: 394.8
+    },
+    {
+    name: "Mt. Everest",
+    dimension: "elevation",
+    length: 9676.29
+    },
+    {
+    name: "Mt. Denali",
+    description: "tallest mountain in the United States",
+    dimension: "elevation",
+    length: 6770
+    },
+    {
+    name: "Rhode Island",
+    dimension: "width",
+    length: 65120
+    }
+    
+]
