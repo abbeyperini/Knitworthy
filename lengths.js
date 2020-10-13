7338ece262b95a3657c4cf5b77b80c2695799f05
abbeyperini <abbeyperini@gmail.com>
2020-10-13 11:40:29 -0400

    lengths library

---
+++ lengths.js
@@ -0,0 +1,202 @@
+// all length units are yards
+
+let lengths = [
+    { 
+    name: "Manhattan Island",
+    description: null,
+    dimension: "width",
+    length: 4048
+    },
+    {
+    name: "an airport runway",
+    description: null,
+    dimension: "length",
+    length: 3663.605
+    },
+    {
+    name: "a football field",
+    description: null,
+    dimension: "length",
+    length: 120.297
+    },
+    {
+    name: "the Symphony of hte Seas",
+    description: "world's largest cruise ship",
+    dimension: "length",
+    length: 394.8
+    },
+    {
+    name: "Mt. Everest",
+    description: null,
+    dimension: "elevation",
+    length: 9676.29
+    },
+    {
+    name: "Mt. Denali",
+    description: "tallest mountain in United States",
+    dimension: "elevation",
+    length: 6770
+    },
+    {
+    name: "Rhode Island",
+    description: null,
+    dimension: "width",
+    length: 65120
+    },
+    {
+    name: "the Steel Dragon 2000",
+    description: "world's largest roller coaster",
+    dimension: "length",
+    length: 2711
+    },
+    {
+    name: "the National Mall",
+    description: "D.C. park home to the Lincoln Memorial and Washington Monument",
+    dimension: "length",
+    length: 3000 
+    },
+    {
+    name: "the Belmont Stakes",
+    description: "horse racing track",
+    dimension: "length",
+    length: 2640
+    },
+    {
+    name: "the Beast",
+    description: "Roller Coaster",
+    dimension: "length",
+    length: 2453 
+    },
+    {
+    name: "the Kentucky Derby",
+    description: "horse racing track",
+    dimension: "length",
+    length: 2200 
+    },
+    {
+    name: "the Hollywood Walk of Fame",
+    description: null,
+    dimension: "length",
+    length: 4300
+    },
+    {
+    name: "the Indianapolis Motor Speedway",
+    description: null,
+    dimension: "length",
+    length: 4400
+    },
+    {
+    name: "the Las Vegas Strip",
+    description: null,
+    dimension: "length",
+    length: 7400
+    },
+    {
+    name: "the Tugela Falls",
+    description: null,
+    dimension: "height",
+    length: 1040
+    },
+    {
+    name: "the Burj Khalifa",
+    description: "the tallest building in the world",
+    dimension: "height",
+    length: 906
+    },
+    {
+    name: "the Marianas Trench",
+    description: "the eepest oceanic trench on Earth",
+    dimension: "depth",
+    length: 11923
+    },
+    {
+    name: "the Empire State Building",
+    description: "a skyscraper in New York City",
+    dimension: "height",
+    length: 416.68
+    },
+    {
+    name: "the Queen Elizabeth 2",
+    description: "a retired British ocean liner converted into a floating hotel",
+    dimension: "length",
+    length: 322 
+    },
+    {
+    name: "the Golden Gate Bridge",
+    description: null,
+    dimension: "height",
+    length: 248
+    },
+    {
+    name: "the Space Needle",
+    description: null,
+    dimension: "height",
+    length: 201
+    },
+    {
+    name: "the St. Paul's Cathedral",
+    description: "an Anglican cathedral in London",
+    dimension: "height",
+    length: 190
+    },
+    {
+    name: "the Washington Monument",
+    description: null,
+    dimension: "height",
+    length: 185.14
+    },
+    {
+    name: "the Spring Temple Buddha",
+    description: "a colossal statue depicting Vairocana Buddha located in the Zhaocun township of Lushan County, Henan, China",
+    dimension: "height",
+    length: 167
+    },
+    {
+    name: "the Great Pyramid of Giza",
+    description: null,
+    dimension: "height",
+    length: 140
+    },
+    {
+    name: "Big Ben",
+    description: null,
+    dimension: "height",
+    length: 100
+    },
+    {
+    name: "the Statue of Liberty",
+    description: null,
+    dimension: "height",
+    length: 101.7
+    },
+    {
+    name: "the Panama Canal",
+    description: null,
+    dimension: "length",
+    length: 96000
+    },
+    {
+    name: "the English Channel",
+    description: null,
+    dimension: "length",
+    length: 117000
+    },
+    {
+    name: "the Cinderella Castle",
+    description: "the icon at the center of Magic Kingdon and Tokya Disneyland",
+    dimension: "height",
+    length: 63
+    },
+    {
+    name: "the Leaning Tower of Pisa",
+    description: null,
+    dimension: "height",
+    length: 61.7
+    },
+    {
+    name: "the Grand Canyon Skywalk",
+    description: "horseshoe-shaped glass bridge extends 70 feet out over the rim of the Grand Canyon",
+    dimension: "height",
+    length: 1200
+    }
+]
