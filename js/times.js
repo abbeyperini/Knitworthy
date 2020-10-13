2b813112dfde49c504d840c75df00822c294412d
brodri4 <70927606+brodri4@users.noreply.github.com>
2020-10-13 16:02:45 -0400

    added times file

---
+++ js/times.js
@@ -0,0 +1,49 @@
+let times = 
+[
+    {
+        task="run a marathon",
+        time=5
+    },
+    {
+        task="drive across the US",
+        time=41
+    },
+    {
+        task="listen to all the Beatles songs",
+        time=11
+    },
+    {
+        task="get to the moon",
+        time=72
+    },
+    {
+        task="build a small home from scratch",
+        time=1536
+    },
+    {
+        task="watch all nine seasons of The Office",
+        time=74
+    },
+    {
+        task="watch the entire Marvel Cinematic Universe",
+        time=50
+    },
+    {
+        task="watch all Harry Potter films",
+        time=20
+    },
+    {
+        task="write a 2-page paper at a slow typing speed",
+        time=3
+    },    {
+        task="watch all of the One Piece anime",
+        time=277
+    },    {
+        task="ride the longest flight in the world from Singapore to Newark",
+        time=19
+    },    {
+        task="go on the longest cruise ride spanning six continents",
+        time=5880
+    }
+
+]
